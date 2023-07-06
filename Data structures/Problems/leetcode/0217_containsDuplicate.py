47880d72c97843f3822c3cd7fee6fed433502143
yutaokkots <yuta.okkotsu@gmail.com>
2023-07-06 12:13:55 -0600

    updated contains-duplicate problem

--- Data structures/Problems/leetcode/0217_containsDuplicate.py
+++ Data structures/Problems/leetcode/0217_containsDuplicate.py
@@ -1,11 +1,26 @@
+# 217. Contains Duplicate
+
+# Given an integer array nums, return true if any value appears at least twice in the array, 
+# and return false if every element is distinct.
+
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def containsDuplicate_1(self, nums: list[int]) -> bool:
         memo = {}
         for n in nums:
             if n not in memo.keys():
                 memo[n] = 1
             else:
-                memo[n] += 1
-            if memo[n] > 1:
                 return True
         return False
+
+    def containsDuplicate_2(self, nums: list[int]) -> bool:
+        nums.sort()
+        for index, num in enumerate(nums):
+            if index == 0:
+                continue
+            if num == nums[index - 1]:
+                return True
+        return False
+    
+    def containsDuplicate_3(self, nums: list[int]) -> bool:
+        return not len(nums) == len(set(nums))
