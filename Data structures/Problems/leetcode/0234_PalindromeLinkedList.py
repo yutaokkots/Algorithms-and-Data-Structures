fb42d40d27add2ca7a1d36d45d9e5b38466c7168
yutaokkots <yuta.okkotsu@gmail.com>
2023-07-08 07:07:47 -0600

    added palindrome linked list

---
+++ Data structures/Problems/leetcode/0234_PalindromeLinkedList.py
@@ -0,0 +1,13 @@
+# Given the head of a singly linked list, return true if it is a 
+# palindrome or false otherwise.
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head: any[ListNode]) -> bool:
+        pass
