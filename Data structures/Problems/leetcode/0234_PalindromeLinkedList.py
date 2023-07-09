d503a05744dea0d5cd5b5ee2408f0fc02b5d4df0
yutaokkots <yuta.okkotsu@gmail.com>
2023-07-09 17:37:54 -0600

    updated palindromLinkedList file to include a test case

--- Data structures/Problems/leetcode/0234_PalindromeLinkedList.py
+++ Data structures/Problems/leetcode/0234_PalindromeLinkedList.py
@@ -1,6 +1,6 @@
 # Given the head of a singly linked list, return true if it is a 
 # palindrome or false otherwise.
-
+import random
 
 # Definition for singly-linked list.
 class ListNode:
@@ -8,6 +8,63 @@ class ListNode:
         self.val = val
         self.next = next
 
-class Solution:
-    def isPalindrome(self, head: any[ListNode]) -> bool:
-        pass
+# class Solution:
+#     def isPalindrome(self, head: any[ListNode]) -> bool:
+#         pass
+
+
+    
+def create_linked_list():
+    #initialize a list that stores the node information 
+    node_list = []
+    # create a node head
+    head = ListNode(random.randint(-100, 100))
+    # determine the length of the linked list
+    length = random.randint(3, 16)
+    for n in range(length):
+        if n == 0:
+            node_list.append(ListNode(head))
+        else:
+            value = random.randint(-100, 100)
+            node = ListNode(value)
+            node_list[-1].next = node
+            node_list.append(node)
+    return node_list[0]
+
+def create_palindromic_linked_list():
+    #initialize a list that stores node information
+    node_list = []
+    #create a node head
+    head = ListNode(random.randint(-100,100))
+    length_half = random.randint(2, 8)
+    for n in range(length_half):
+        if n == 0:
+            node_list.append(ListNode(head))
+        else:
+            value = random.randint(-100,100)
+            node = ListNode(value)
+            node_list[-1].next = node 
+            node_list.append(node)
+    size = len(node_list) - 1
+    for n in range(size, -1, -1):
+        node = ListNode(node_list[n].val)
+        if node_list[n] == node_list[-1]:
+            node_list[n].next = node
+            node_list.append(node)
+        else:
+            node_list[-1].next = node
+            node_list.append(node)
+    
+
+    return node_list[0]
+
+head_1 = ref_1 = create_linked_list()
+head_2 = ref_2 = create_palindromic_linked_list()
+
+def read_linked_list(head):
+    while head.next:
+        print(head.val)
+        head = head.next
+
+read_linked_list(ref_1)
+read_linked_list(ref_2)
