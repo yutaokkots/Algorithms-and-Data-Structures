c553e5cf55a7aba6e4c6b89b69d437d903fdbf3d
yutaokkots <yuta.okkotsu@gmail.com>
2023-07-10 00:38:03 -0600

    updated validPalindrome2.py code to include soln

--- Data structures/Problems/leetcode/0680_validPalindrome2.py
+++ Data structures/Problems/leetcode/0680_validPalindrome2.py
@@ -2,3 +2,21 @@
 
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+
+        # bc d eed a cb
+        # 01 2 345 6 78
+        #    d eed a
+        #    d eed    -> dee d
+        #      eed a  -> a dee
+
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                left_slice = s[l:r]     
+                right_slice = s[l+1:r+1]
+                return left_slice == left_slice[-1::-1] or right_slice == right_slice[-1::-1]
+            else: # if s[l] == s[r]
+                l += 1
+                r -= 1
+        
+        return True 
