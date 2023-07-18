2e6ff49fa68e75164a9a38ec81f95b65c52f1e08
yutaokkots <yuta.okkotsu@gmail.com>
2023-07-17 23:38:05 -0600

    updated largestRectangle, and looping nested lists

--- Data structures/Problems/leetcode/0084_largestRectangleInHistogram.py
+++ Data structures/Problems/leetcode/0084_largestRectangleInHistogram.py
@@ -4,18 +4,43 @@
 class Solution:
     def largestRectangleArea(self, heights: list[int]) -> int:
         maxArea = 0
-        stack = []
+        stack = [] # pair of elements, [index, height]
 
         for i, h in enumerate(heights):
             start = i
+            # while the stack is not empty, 
+            # and the current height is greater than the last item of the stack's height
             while stack and stack[-1][1] > h:
+                # destructure the popped item 
                 index, height = stack.pop()
+                #check if this popped stack could be the largest rectangle
+                # compare current maxArea, and area (height x (i - index))
                 maxArea = max(maxArea, height * (i - index))
+                # because we know that this popped height, 'height', is greater than the current height, 'h'
+                # we can extend the start index, 'start', to the index that was just popped, 'index'
                 start = index
-            stack.append((start, h))
-        for i, h in stack:
-            maxArea = max(maxArea, h * (len(heights) - i))
-        return maxArea
+                # when adding the current index and height [index, height],
+                # instead of adding the current index, use the 'start' index that was reassigned above. 
+                stack.append((start, h))
+            #still compute the heights for items left in histogram
+            for i, h in stack:
+                maxArea = max(maxArea, h * (len(heights) - i))
+
+
+
+        # maxArea = 0
+        # stack = []
+
+        # for i, h in enumerate(heights):
+        #     start = i
+        #     while stack and stack[-1][1] > h:
+        #         index, height = stack.pop()
+        #         maxArea = max(maxArea, height * (i - index))
+        #         start = index
+        #     stack.append((start, h))
+        # for i, h in stack:
+        #     maxArea = max(maxArea, h * (len(heights) - i))
+        # return maxArea
 
 
 
---
+++ Data structures/Problems/sandbox/loopingNestedlist.py
@@ -0,0 +1,38 @@
+# what is the difference between for looping and enumerating?
+# ability to destructure a list of a list while looping
+
+# values of heights in a list
+heights = [2,1,5,6,2,3]
+# values of heights and their index, represented by [index, height], in a variable called stack
+stack = [[0,2],[1,1],[2,5],[3,6],[4,2],[5,3]]
+
+# values of heights and their index, and another value, 'width, represented by [index, height, width], 
+# in a variable called stack_with_three
+stack_with_three = [[0,2,6],[1,1,3],[2,5,2],[3,6,8],[4,2,2],[5,3,1]]
+
+for i, h in enumerate(heights):
+    print(f"index: {i}; height: {h}")
+# index: 0; height: 2
+# index: 1; height: 1
+# index: 2; height: 5
+# index: 3; height: 6
+# index: 4; height: 2
+# index: 5; height: 3
+
+for i, h in stack:
+    print(f"index: {i}; height: {h}")
+# index: 0; height: 2
+# index: 1; height: 1
+# index: 2; height: 5
+# index: 3; height: 6
+# index: 4; height: 2
+# index: 5; height: 3
+
+for i, h, w in stack_with_three:
+    print(f"index: {i}; height: {h}, width: {w}")
+# index: 0; height: 2, width: 6
+# index: 1; height: 1, width: 3
+# index: 2; height: 5, width: 2
+# index: 3; height: 6, width: 8
+# index: 4; height: 2, width: 2
+# index: 5; height: 3, width: 1
