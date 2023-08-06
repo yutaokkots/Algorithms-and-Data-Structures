00a2d0f24c4b51420656273d28990e7d859553a5
yutaokkots <yuta.okkotsu@gmail.com>
2023-08-05 23:46:16 -0600

    backbone for sortStack

---
+++ Data structures and Algorithms/Problems/CTCI/ch03-StacksAndQueues/ch03-05-sortStack.py
@@ -0,0 +1,28 @@
+# Write a program to sort a stack such that the smallest items are on top. 
+# you can use an additional temporary stack, but you may not copy the elements
+# into any other data structure (such as an array)
+# The stack supports the following operations:
+# push
+# pop
+# peek
+# isEmpty
+
+class sortStack():
+    def __init__(self):
+        self.main_stack = []
+        self.temp_stack = []
+
+    def sort(self):
+        pass
+
+    def push(self, val:int):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self) -> int:
+        pass
+
+    def isEmpty(self) -> bool:
+        pass
