ff94d0d413883f2e0ef7e7423e00d0a7a61b79f8
yutaokkots <yuta.okkotsu@gmail.com>
2023-08-06 00:36:22 -0600

    updated sorted stack class

--- Data structures and Algorithms/Problems/CTCI/ch03-StacksAndQueues/ch03-05-sortStack.py
+++ Data structures and Algorithms/Problems/CTCI/ch03-StacksAndQueues/ch03-05-sortStack.py
@@ -7,22 +7,74 @@
 # peek
 # isEmpty
 
-class sortStack():
+class SortStack():
     def __init__(self):
         self.main_stack = []
         self.temp_stack = []
 
-    def sort(self):
-        pass
+    def sort(self, val:int):
+        # [5, 4, 3, 2]
+        # [2, 3, 4, 5]
+        # [2, 3, 4, 5, 1]
+        # 1
+        while self.main_stack:
+            popped_element = self.main_stack.pop()
+            self.temp_stack.append(popped_element)
+        self.temp_stack.append(val)
+        print(self.temp_stack)
+        while self.temp_stack:
+            if len(self.temp_stack) < 2:
+                self.main_stack.append(self.temp_stack.pop())
+                break
+            curr_val = self.temp_stack.pop()
+            next_val = self.temp_stack.pop()
+            if curr_val < next_val:
+                self.main_stack.append(next_val)
+                self.temp_stack.append(curr_val)
+            else:
+                self.main_stack.append(curr_val)
+                self.main_stack.append(next_val)
+
 
     def push(self, val:int):
-        pass
+        self.sort(val)
 
-    def pop(self):
-        pass
+    def pop(self) -> int:
+        if self.main_stack == []:
+            return -1
+        return self.main_stack.pop()
 
     def peek(self) -> int:
-        pass
+        print(self.main_stack)
 
     def isEmpty(self) -> bool:
-        pass
+        return len(self.main_stack) == 0
+
+sort_stack = SortStack()
+sort_stack.push(5)
+sort_stack.push(4)
+sort_stack.push(6)
+sort_stack.push(2)
+sort_stack.push(3)
+sort_stack.push(9)
+sort_stack.push(7)
+print(f"main: {sort_stack.main_stack}, temp: {sort_stack.temp_stack}")
+a = sort_stack.pop()
+b = sort_stack.pop()
+c = sort_stack.pop()
+
+print(f"a: {a}, b: {b}, c: {c}")
+
+sort_stack.peek()
+
+print(f"is empty?: {sort_stack.isEmpty()}")
+sort_stack.pop()
+print(f"is empty?: {sort_stack.isEmpty()}")
+sort_stack.pop()
+print(f"is empty?: {sort_stack.isEmpty()}")
+sort_stack.pop()
+print(f"is empty?: {sort_stack.isEmpty()}")
+sort_stack.pop()
+print(f"is empty?: {sort_stack.isEmpty()}")
+sort_stack.pop()
+print(f"is empty?: {sort_stack.isEmpty()}")
