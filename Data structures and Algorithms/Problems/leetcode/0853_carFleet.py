6c07a25298fc7ec3d4f74fac95c609221d70d8c6
yutaokkots <yuta.okkotsu@gmail.com>
2023-08-22 17:33:46 -0600

    updated reverse linked list problem

--- Data structures and Algorithms/Problems/leetcode/0853_carFleet.py
+++ Data structures and Algorithms/Problems/leetcode/0853_carFleet.py
@@ -10,9 +10,45 @@ class Solution:
         
         return len(stack)
 
+    def carFleet1(self, target: int, position: list[int], speed: list[int]) -> int:
+        car_info = []
+
+        for i, p in enumerate(position):
+            d = target - p
+            car_info.append((d, d / speed[i]))
+
+        car_info.sort()
+
+        stack = []
+        
+        for car in car_info:
+            if stack == []:
+                stack.append(car)
+            if stack and car[1] > stack[-1][1]:
+                stack.append(car)
+            else:
+                continue
+
+        return len(stack)
 
 t = [3, 5, 2]
 p = [3, 2, 1]
 
 sol = Solution()
 sol.carFleet()
+
+
+        # target = 12
+        # [10,8, 0, 5,3] <- position, 
+        # [2, 4, 1, 1,3] <- speed
+        # [2, 4,12, 7,9] <- distance
+        # [1, 1,12, 7,3]
+        #sorted
+        # distance:
+        # [12,9, 7, 4,  2] <- distance
+        # [0, 3, 5, 8, 10] -> 12
+        # [1, 3, 1, 4,  2] <- speed
+        # [12,3, 7, 1,  1] <- time
+        #  speed = distance / time
+        #   distance / speed = time
+        # 
---
+++ Data structures and Algorithms/Problems/leetcode/_206_reverseLinkedList.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import ListNode
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Purpose: output the input linked list in reverse
+        # linked list can be empty
+        # number of nodes can be up to 5000
+        # the value of the nodes is between -5000 and + 5000
+
+        #edge case where head is empty
+        if not head:
+            return head
+
+        # create the first Node 
+        new_node = ListNode(head.val)
+
+        #[1, 2, 3, 4, 5]
+        while head.next != None:
+            head = head.next
+            new_node = ListNode(head.val, new_node) 
+
+        # specify returning:
+        return new_node
+    
+    def reverseList2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return head
+        previous_head = None
+        while head.next != None:
+            next_ = head.next    
+            head.next = previous_head
+            previous_head = head  
+            head = next_            
+        head.next = previous_head
+        return head
