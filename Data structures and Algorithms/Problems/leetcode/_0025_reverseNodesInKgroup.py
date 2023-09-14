a5eab09ec3d47a88e1e1b7395dc9738a2906f93c
yutaokkots <yuta.okkotsu@gmail.com>
2023-09-14 08:48:56 -0600

    updated Reverse Nodes in K group

--- Data structures and Algorithms/Problems/leetcode/_0025_reverseNodesInKgroup.py
+++ Data structures and Algorithms/Problems/leetcode/_0025_reverseNodesInKgroup.py
@@ -21,9 +21,31 @@ class ListNode:
         self.next = next
 
 '''
+from ListNode import ListNode
+from typing import Optional
+
+NODE_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+def linkedListGenerator(lst):
+    head = node = ListNode(lst[0])
+    for i in range(len(lst)):
+        if i == 0:
+            continue
+        node.next = ListNode(lst[i])
+        node = node.next
+    return head
+
+def linkedListReader(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
 
 
 class Solution:
+    '''
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)
         groupPrev = dummy
@@ -51,3 +73,41 @@ class Solution:
             curr = curr.next
             k -= 1
         return curr
+    ''' 
+
+
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = previousGroup = ListNode(next=head)
+        while True:
+            kth_node = self.get_kth_node(previousGroup, k)
+            if kth_node is None:
+                break
+            nextGroup = kth_node.next
+            current, previous = previousGroup.next, nextGroup
+            while nextGroup != current:
+                temp = current.next
+                current.next = previous
+                previous = current
+                current = temp
+            
+            tmp = previousGroup.next
+
+            previousGroup.next = kth_node
+            previousGroup = tmp
+            
+        return dummy.next
+    
+    def get_kth_node(self, start_node, k):
+        while start_node and k > 0:
+            start_node = start_node.next
+            k -= 1
+
+        return start_node
+
+
+head_1 = linkedListGenerator(NODE_1)
+
+sol = Solution()
+head_1_processed = sol.reverseKGroup(head=head_1, k=3)
+
+print(linkedListReader(head_1_processed))
