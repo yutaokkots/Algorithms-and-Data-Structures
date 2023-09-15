f12ee4739b8d1fa64d244f0523769e7f3cb56f1e
yutaokkots <yuta.okkotsu@gmail.com>
2023-09-15 12:09:24 -0600

    updated diameter of binary tree

--- Data structures and Algorithms/Problems/leetcode/_0104_maxDepthBinaryTree.py
+++ Data structures and Algorithms/Problems/leetcode/_0104_maxDepthBinaryTree.py
@@ -22,6 +22,13 @@ class TreeNode:
         self.left = left
         self.right = right
 '''
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
