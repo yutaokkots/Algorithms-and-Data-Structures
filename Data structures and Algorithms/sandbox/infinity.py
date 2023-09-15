16eadba4add530521cd8fb357bbe0e24fbc5c27d
yutaokkots <yuta.okkotsu@gmail.com>
2023-09-14 23:05:45 -0600

    updated median of two sorted arrays

---
+++ Data structures and Algorithms/sandbox/infinity.py
@@ -0,0 +1,10 @@
+# float("-infinity")
+# setting infinity
+
+
+inf_negative = float("-infinity")
+inf_positive = float("infinity")
+
+test1 = max(inf_positive, 500)
+
+print(f"Value of 'test1': {test1}")
