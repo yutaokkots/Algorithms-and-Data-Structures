28c4c69ca728f1f6648e4083445e50b2aedf1792
yutaokkots <yuta.okkotsu@gmail.com>
2023-09-18 20:41:01 -0600

    updated balanced binary tree

--- Data structures and Algorithms/Problems/leetcode/_0110_balancedBinaryTree.py
+++ Data structures and Algorithms/Problems/leetcode/_0110_balancedBinaryTree.py
@@ -4,6 +4,7 @@ Easy
 
 Given a binary tree, determine if it is height-balanced.
 
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
 
 Example 1:
 Input: root = [3,9,20,null,null,15,7]
@@ -34,3 +35,31 @@ class TreeNode:
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
 
+        def dfs(root):
+            if not root:
+                return [True, 0]
+            left = dfs(root.left)
+            right = dfs(root.right)
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
+    
+class Solution2:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.result = True
+        self.depth = 0
+
+        self.recursive(root)
+        return self.result
+
+    def recursive(self, node):
+        if not node:
+            return 0
+
+        left = self.recursive(node.left)
+        right = self.recursive(node.right)
+        self.depth = max(left, right) + 1
+        if abs(left - right) > 1:
+            self.result = False
+        return self.depth
--- Data structures and Algorithms/Problems/leetcode/_0543_diameterOfBinaryTree.py
+++ Data structures and Algorithms/Problems/leetcode/_0543_diameterOfBinaryTree.py
@@ -49,3 +49,20 @@ class Solution:
         
         dfs(root)
         return result[0]
+    
+class Solution2:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.max = 0
+        self.recursive(root)
+        return self.max
+
+    def recursive(self, node):
+        if not node:
+            return 0
+
+        left_depth = self.recursive(node.left)
+        right_depth = self.recursive(node.right)
+
+        self.max = max(self.max, left_depth + right_depth)
+
+        return 1 + max(left_depth, right_depth)
