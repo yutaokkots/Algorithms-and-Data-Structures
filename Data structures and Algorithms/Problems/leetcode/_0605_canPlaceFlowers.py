1d01fbfe6c040fc36e21b9e2a94d76e16d06173e
yutaokkots <yuta.okkotsu@gmail.com>
2023-10-07 00:24:52 -0600

    updated can place flower bed

---
+++ Data structures and Algorithms/Problems/leetcode/_0605_canPlaceFlowers.py
@@ -0,0 +1,39 @@
+'''
+605. Can Place Flowers
+Easy
+
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+
+
+
+
+'''
+from typing import List
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+        i = 0
+        while i < len(flowerbed):
+            if flowerbed[i] == 0:
+                if i == 0 or flowerbed[i - 1] == 0:
+                    if i == len(flowerbed) - 1 or flowerbed[i + 1] == 0:
+                        flowerbed[i] = 1
+                        count += 1
+            i += 1
+        
+        return count >= n
+        
