2b08141c175e35f87b564ea436e5b8c18fc4fe3c
yutaokkots <yuta.okkotsu@gmail.com>
2023-10-18 18:11:58 -0600

    number of islands updated

---
+++ Data structures and Algorithms/Problems/leetcode/_0200_numberOfIslands.py
@@ -0,0 +1,67 @@
+'''
+200. Number of Islands
+Medium
+
+--
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+--
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+
+'''
+from typing import List
+import collections
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        rows, cols = len(grid), len(grid[0])
+        # mark where we have visited
+        visit = set()
+        islands = 0
+
+        def bfs(r,c):
+            q = collections.deque()
+            visit.add((r, c))
+            q.append((r, c))
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r in range(rows) and
+                        c in range(cols) and
+                        grid[r][c] == "1" and
+                        (r, c) not in visit):
+                        q.append((r, c))
+                        visit.add((r, c))
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1" and (row, col) not in visit:
+                    bfs(row, col)
+                    islands += 1
+        return islands
