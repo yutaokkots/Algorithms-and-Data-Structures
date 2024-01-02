43177f94f4df53f68bbd519147613695476994bf
yutaokkots <yuta.okkotsu@gmail.com>
2024-01-02 01:46:32 -0700

    Updated attempt at LC2945.

---
+++ Data structures and Algorithms/Problems/leetcode/_2945_find_max_non_decreasing_array_length.py
@@ -0,0 +1,86 @@
+"""2945. Find Maximum Non-decreasing Array Length"""
+
+"""
+2945. Find Maximum Non-decreasing Array Length
+Hard
+
+You are given a 0-indexed integer array nums.
+
+You can perform any number of operations, where each operation involves selecting a subarray of the array and replacing it with the sum of its elements. For example, if the given array is [1,3,5,6] and you select subarray [3,5] the array will convert to [1,8,6].
+
+Return the maximum length of a non-decreasing array that can be made after applying operations.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+
+Input: nums = [5,2,2]
+Output: 1
+Explanation: This array with length 3 is not non-decreasing.
+We have two ways to make the array length two.
+First, choosing subarray [2,2] converts the array to [5,4].
+Second, choosing subarray [5,2] converts the array to [7,2].
+In these two ways the array is not non-decreasing.
+And if we choose subarray [5,2,2] and replace it with [9] it becomes non-decreasing. 
+So the answer is 1.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 4
+Explanation: The array is non-decreasing. So the answer is 4.
+Example 3:
+
+Input: nums = [4,3,2,6]
+
+Output: 3
+Explanation: Replacing [3,2] with [5] converts the given array to [4,5,6] that is non-decreasing.
+Because the given array is not non-decreasing, the maximum possible answer is 3.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
+"""
+
+from typing import List
+
+class Solution:
+    """Solution class for LC2945."""
+
+    def findMaximumLength(self, nums: List[int]) -> int:
+        """Class method to find the max length and solve LC2945."""
+
+        # Incorrect solution:
+        self.nums = nums
+        length = len(nums)
+        if length == 1:
+            return length
+        while not self.check_non_decreasing(self.nums):
+            temp = []
+            l = 0
+            r = 0
+            while l < length and r <= length:
+                if not temp:
+                    temp.append(nums[r])
+                    l += 1
+                    r = l + 1
+                    continue
+                if sum(nums[l:r]) <= temp[-1]:
+                    r += 1
+                    continue
+                temp.append(sum(nums[l:r]))
+                l = r
+            self.nums = temp
+        return len(self.nums)
+    
+    def check_non_decreasing(self, lst:List[int]):
+        if len(lst) == 1:
+            return True
+        for i, n in enumerate(lst):
+            if i == 0:
+                continue
+            if lst[i - 1] >= n:
+                return False
+        return True
---
+++ Data structures and Algorithms/Problems/leetcode/test/test__2945_find_max_non_decreasing_array_length.py
@@ -0,0 +1,50 @@
+"""Test case for LC2945"""
+from unittest import TestCase
+from leetcode._2945_find_max_non_decreasing_array_length import Solution
+
+TEST_CASES2945 = [
+    [[5,2,2], 1],
+    [[1,2,3,4], 4],
+    [[4,3,2,6], 3],
+    [[1], 1],
+    [[1,4,3,2,1], 3],
+    [[1,1,1,1,1], 2],
+    [[1,1,1,1,1,1], 3],
+    [[1,1,1,1,1,1,1], 3],
+    [[5,1,1,1,1,1,1], 2],
+    [[4,3,2,6,7], 4],
+    [[4,3,2,6,1], 3],
+    [[4,3,3,6,1], 3],
+    [[4,3,3,3,6,1], 3],
+    [[4,3,1,1,6,1,9], 4],
+    [[272,482,115,925,983], 4]
+]
+
+TEST_NON_DECREASING = [
+    [[1,2,3,4], True],
+    [[1,2,3,6], True],
+    [[3,4,5,10], True],
+    [[1,2,1,2], False],
+    [[5,2,3,4], False],
+    [[10], True],
+]
+
+class Test2945(TestCase):
+    """Test class for LC2945."""
+    def setUp(self):
+        self.soln = Solution()
+        self.test_cases = TEST_CASES2945
+        self.test_nd = TEST_NON_DECREASING
+    
+    def test_nondecreasing(self):
+        for case, answer in self.test_nd:
+            self.assertEqual(self.soln.check_non_decreasing(case), answer)
+
+    def testcases(self):
+        for idx, case_set in enumerate(self.test_cases):
+            case, answer = case_set
+            with self.subTest(f"Subtest {idx+1}"):
+                print(f"\nSubtest {idx+1}: {case}")
+                self.assertEqual(type(answer),int)
+                print(self.soln.findMaximumLength(case))
+                self.assertEqual(self.soln.findMaximumLength(case), answer)
