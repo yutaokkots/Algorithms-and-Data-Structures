4ac0b195ac5b3411ef2b503dcbe8a0a4d6559592
yutaokkots <yuta.okkotsu@gmail.com>
2024-01-09 00:21:16 -0700

    Updated LC0455 code.

--- Data structures and Algorithms/Problems/leetcode/_0455_assignCookies.py
+++ Data structures and Algorithms/Problems/leetcode/_0455_assignCookies.py
@@ -21,14 +21,75 @@ Output: 2
 Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2. 
 You have 3 cookies and their sizes are big enough to gratify all of the children, 
 You need to output 2.
- 
 
 Constraints:
     1 <= g.length <= 3 * 10^4
     0 <= s.length <= 3 * 10^4
     1 <= g[i], s[j] <= 2^31 - 1
 """
-from typing import List
+from typing import List, Dict
+from collections import Counter
+
 class Solution:
+    """Optimized solution for LC0455
+
+    Big-O notation
+    --------------
+    time: O(m * log(m) + n * log(n))
+    space: O(1).
+    """
+
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        
+        g.sort()
+        s.sort()
+        i = 0
+        count = 0
+        for greedy in g:
+            if i >= len(s):
+                break
+            while i < len(s) and greedy > s[i]:
+                i += 1
+            if i < len(s) and greedy <= s[i]:
+                count += 1
+                i += 1
+        return count
+
+class Solution2:
+    """Naive solution for LC0455.
+
+    Big-O notation
+    --------------
+    time: O(m + n + m * n) 
+    space: O(m + n)
+    """
+
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        if not g or not s:
+            return 0
+        greed_counter = Counter(g)
+        size_counter = Counter(s)
+        size_max = max(s)
+        counter = 0
+        for size in g: 
+            if (size in size_counter.keys() and 
+                    greed_counter[size] > 0 and
+                    size_counter[size] > 0):
+                greed_counter[size] -= 1
+                size_counter[size] -= 1
+                counter += 1
+                continue
+            s_num = self.linear_search(s, size_counter, size, size_max)
+            if s_num > -1:
+                greed_counter[s_num] -= 1
+                size_counter[s_num] -= 1
+                counter += 1
+        return counter
+
+    def linear_search(self, lst:List[int], dict:Dict[int, int], target:int, target_max:int) -> int:
+        for t in range(target, target_max + 1):
+            if t in lst and self.available(dict, t):
+                return t
+        return -1
+
+    def available(self, dict:Dict[int, int], target:int) -> bool:
+        return dict[target] > 0
--- Data structures and Algorithms/Problems/leetcode/test/test__0455_assignCookies.py
+++ Data structures and Algorithms/Problems/leetcode/test/test__0455_assignCookies.py
@@ -1,7 +1,12 @@
 """Testcase for LC455 - Assign Cookies"""
 
+"""
+Problems % python3 -m unittest leetcode.test.test__0455_assignCookies
+"""
+
 from unittest import TestCase
 from leetcode._0455_assignCookies import Solution
+from .lc0455_test_cases import test_case_05, test_case_06
 
 TEST_CASES = [
     {
@@ -13,9 +18,25 @@ TEST_CASES = [
         "g": [1,2],
         "s": [1,2,3],
         "answer": 2
+    },
+    {
+        "g": [1,2,2],
+        "s": [2,1,1],
+        "answer": 2
+    },
+    {
+        "g": [2,2],
+        "s": [2,1,3],
+        "answer": 2
+    },
+    test_case_05,
+    test_case_06,
+    {
+        "g": [2,3,5],
+        "s": [2,1,4,4],
+        "answer": 2
     }
 ]
-
 
 class TestLC455(TestCase):
     """Test class for LC455 """
@@ -25,5 +46,6 @@ class TestLC455(TestCase):
         self.sol = Solution()
 
     def test_cases(self):
-        for case in self.test_cases:
-            self.assertEqual(self.sol.findContentChildren(g=case["g"], s=case["s"]), case["answer"])
+        for i, case in enumerate(self.test_cases):
+            with self.subTest(f"Test case {i + 1}"):
+                self.assertEqual(self.sol.findContentChildren(g=case["g"], s=case["s"]), case["answer"])
