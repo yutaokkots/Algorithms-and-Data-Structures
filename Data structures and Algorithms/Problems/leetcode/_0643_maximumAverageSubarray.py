787b715ec6bfba507a58e108358c4fe59a7aad66
yutaokkots <yuta.okkotsu@gmail.com>
2024-01-11 01:27:44 -0700

    Updated the code and notes from LC0643, LC1456, and LC1732.

---
+++ Data structures and Algorithms/Problems/leetcode/_0643_maximumAverageSubarray.py
@@ -0,0 +1,44 @@
+"""643. Maximum Average Subarray I"""
+
+"""
+643. Maximum Average Subarray I
+Easy
+
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+Example 1:
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+Example 2:
+Input: nums = [5], k = 1
+Output: 5.00000
+ 
+Constraints:
+    n == nums.length
+    1 <= k <= n <= 10^5
+    -104 <= nums[i] <= 10^4
+
+"""
+
+from typing import List
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        if len(nums) == 1:
+            return nums[0]
+        left = 0
+        prev = 0
+        max_avg = float("-inf")
+        summation = sum(nums[left:left+k])
+        for right in range(k-1, len(nums)):
+            if left > 0:
+                summation = summation - nums[prev] + nums[right]
+                prev += 1
+            avg = summation / k
+            max_avg = max(max_avg, avg)
+            left += 1
+        return max_avg
