bc30615c6e41499d297b09756f2f403838f59f0d
yutaokkots <yuta.okkotsu@gmail.com>
2024-01-19 13:31:25 -0700

    Updated LC1493.

--- data_structures_and_algorithms/Problems/leetcode/_1004_maxConsecutiveOnesIII.py
+++ data_structures_and_algorithms/Problems/leetcode/_1004_maxConsecutiveOnesIII.py
@@ -4,6 +4,10 @@
 1004. Max Consecutive Ones III
 Medium
 
+#####
+Also see: 1493. Longest Subarray of 1's After Deleting One Element"
+#####
+
 Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
 
 Example 1:
