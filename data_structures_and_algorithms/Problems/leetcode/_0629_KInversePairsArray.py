7f7aac41d5657535f3f80d106d3a1fdd34c14d6e
yutaokkots <yuta.okkotsu@gmail.com>
2024-01-27 12:56:35 +0900

    Updated solution for LC0629.

--- data_structures_and_algorithms/Problems/leetcode/_0629_KInversePairsArray.py
+++ data_structures_and_algorithms/Problems/leetcode/_0629_KInversePairsArray.py
@@ -25,16 +25,16 @@ Constraints:
 
 class Solution:
     """Solution class for LC629 adapted from user @Recode_-1"""
-    def kInversePairs(self, n: int, k: int) -> int:
+    def kInversePairs(self, n: int, k: int) -> int:    
         modulo = 10**9 + 7
         dp = [[0] * (k + 1) for _ in range(n + 1)]
+
         for i in range(1, n + 1):
             for j in range(k + 1):
                 if j == 0:
                     dp[i][j] = 1
                 else:
-                    value = (dp[i-1][j] + modulo - 
-                            (dp[i-1][j-1]
-                            if j - i >=0 else 0)) % modulo
-                    dp[i][j] = (dp[i][j - 1] + value) % modulo
+                    val = (dp[i - 1][j] + modulo - (dp[i - 1][j - i] if j - i >= 0 else 0)) % modulo
+                    dp[i][j] = (dp[i][j - 1] + val) % modulo
+
         return (dp[n][k] + modulo - (dp[n][k - 1] if k > 0 else 0)) % modulo
