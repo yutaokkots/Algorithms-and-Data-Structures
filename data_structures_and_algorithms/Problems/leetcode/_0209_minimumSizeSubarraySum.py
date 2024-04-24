bbc2e4c3a5b51ed021c3ff04013e546fb9b7592e
yutaokkots <yuta.okkotsu@gmail.com>
2024-04-23 22:07:34 -0600

    Updated unittest and solution for LC0209.

---
+++ data_structures_and_algorithms/Problems/leetcode/_0209_minimumSizeSubarraySum.py
@@ -0,0 +1,43 @@
+'''
+209. Minimum Size Subarray Sum
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray
+whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+Example 1:
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+Example 2:
+Input: target = 4, nums = [1,4,4]
+Output: 1
+
+Example 3:
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0 
+
+Constraints:
+    1 <= target <= 10^9
+    1 <= nums.length <= 10^5
+    1 <= nums[i] <= 10^4
+'''
+
+from typing import List
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        if sum(nums) < target:
+            return 0
+        summ = 0
+        left = 0
+        min_length = len(nums) 
+        for right in range(len(nums)):
+            summ += nums[right]
+            while summ >= target:
+                summ -= nums[left]
+                min_length = min(min_length, right + 1 - left)
+                left += 1
+
+        return min_length 
